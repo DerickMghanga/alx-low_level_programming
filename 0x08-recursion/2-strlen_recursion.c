b55198403eb21a8fde4c704e09b4157059a30226
DerickMghanga <derickmghanga3329@gmail.com>
2023-04-06 18:54:19 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,14 +6,14 @@
   *Return: The length of the string
   */
 
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
 {
 	int longit - 0;
 
 	if (*s)
 	{
 		longit++;
-		longit + -_strlen_recursion(s + 1);
+		longit +-_strlen_recursion(s + 1);
 	}
 	return (longit);
 }
